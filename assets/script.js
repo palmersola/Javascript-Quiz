e8567568a9d9673b36ebeb2d7f7c643610d6dfff
palmersola <palmersola@yahoo.com>
2022-06-29 23:19:47 -0500

    added questions array to js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,41 @@
-var questions = [(q1 = {}), (q2 = {})];
+var questions = [
+  (q1 = {
+    question: "",
+    answer: "",
+    answer: "",
+    answerCorrect: "",
+    answer: ""
+  }),
+  (q2 = {
+    question: "",
+    answerCorrect: "",
+    answer: "",
+    answer: "",
+    answer: ""
+  }),
+  (q3 = {
+    question: "",
+    answer: "",
+    answer: "",
+    answerCorrect: "",
+    answer: ""
+  }),
+  (q4 = {
+    question: "",
+    answer: "",
+    answer: "",
+    answer: "",
+    answerCorrect: ""
+  })(
+    (q5 = {
+      question: "",
+      answer: "",
+      answerCorrect: "",
+      answer: "",
+      answer: ""
+    })
+  )
+];
 
 // start function
 
